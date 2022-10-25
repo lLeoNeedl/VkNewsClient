358958c97eb7fcd6c678c9fae971b9656ff96d96
a.sumin <sumin93@mail.ru>
2022-10-26 00:12:43 +0300

    Adding comments screen

--- app/src/main/java/com/sumin/vknewsclient/domain/PostComment.kt
+++ app/src/main/java/com/sumin/vknewsclient/domain/PostComment.kt
@@ -5,6 +5,7 @@ import com.sumin.vknewsclient.R
 data class PostComment(
     val id: Int,
     val authorName: String = "Author",
-    val authorAvatarId: Int = R.drawable.
-
+    val authorAvatarId: Int = R.drawable.comment_author_avatar,
+    val commentText: String = "Long comment text",
+    val publicationDate: String = "14:00"
 )
