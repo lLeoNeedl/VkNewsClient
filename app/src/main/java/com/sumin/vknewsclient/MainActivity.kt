5c66461042d613ef9d6c9442743fd1e647124935
Leonid Lavrov <274852@gmail.com>
2023-04-03 23:13:26 +0300

    Working with State

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -13,81 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sumin.vknewsclient.ui.theme.MainScreen
 import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test()
+            VkNewsClientTheme {
+                MainScreen()
+            }
         }
     }
 }
 
-@Preview
-@Composable
-private fun Test() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "TopAppBar title")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            BottomNavigation {
-                BottomNavigationItem(
-                    icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)},
-                    label = {Text("Favourite")},
-                    selected = false,
-                    onClick = { /*TODO*/ }
-                )
-                BottomNavigationItem(
-                    icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)},
-                    label = {Text("Favourite")},
-                    selected = false,
-                    onClick = { /*TODO*/ }
-                )
-                BottomNavigationItem(
-                    icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)},
-                    label = {Text("Favourite")},
-                    selected = false,
-                    onClick = { /*TODO*/ }
-                )
-            }
-        },
-        drawerContent = {
-            NavigationRail {
-                NavigationRailItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    icon = { Icon(imageVector = Icons.Filled.Search, contentDescription = null)},
-                    label = { Text(text = "Search")}
-                )
-                NavigationRailItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    icon = { Icon(imageVector = Icons.Filled.Add, contentDescription = null)},
-                    label = { Text(text = "Add")}
-                )
-                NavigationRailItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    icon = { Icon(imageVector = Icons.Filled.Settings, contentDescription = null)},
-                    label = { Text(text = "Settings")}
-                )
-            }
-        }
-    ) {
-        Text(
-            modifier = Modifier.padding(it),
-            text = "This is scaffold content"
-        )
-    }
-}
