b4e8cb8a20e7b041ea2daf396621ed67f4b36759
Leonid Lavrov <274852@gmail.com>
2023-04-18 21:19:16 +0300

    Temporary commit

---
+++ app/src/main/java/com/sumin/vknewsclient/domain/PostComment.kt
@@ -0,0 +1,10 @@
+package com.sumin.vknewsclient.domain
+
+import com.sumin.vknewsclient.R
+
+data class PostComment(
+    val id: Int,
+    val authorName: String = "Author",
+    val authorAvatarId: Int = R.drawable.
+
+)
