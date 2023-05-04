5dccb9b826ad2ab4d34b5c8e7c172dc365cb4d5a
Leonid Lavrov <274852@gmail.com>
2023-05-04 22:14:32 +0300

    Adding loading data when scrolling news feed

--- app/src/main/java/com/sumin/vknewsclient/presentation/news/NewsFeedScreen.kt
+++ app/src/main/java/com/sumin/vknewsclient/presentation/news/NewsFeedScreen.kt
@@ -2,21 +2,28 @@ package com.sumin.vknewsclient.presentation.news
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.DismissDirection
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sumin.vknewsclient.domain.FeedPost
+import com.sumin.vknewsclient.ui.theme.DarkBlue
 
 @Composable
 fun NewsFeedScreen(
@@ -33,7 +40,8 @@ fun NewsFeedScreen(
                 posts = currentState.posts,
                 viewModel = viewModel,
                 paddingValues = paddingValues,
-                onCommentClickListener = onCommentClickListener
+                onCommentClickListener = onCommentClickListener,
+                nextDataIsLoading = currentState.nextDataIsLoading
             )
         }
 
@@ -47,7 +55,8 @@ private fun FeedPosts(
     posts: List<FeedPost>,
     viewModel: NewsFeedViewModel,
     paddingValues: PaddingValues,
-    onCommentClickListener: (FeedPost) -> Unit
+    onCommentClickListener: (FeedPost) -> Unit,
+    nextDataIsLoading: Boolean
 ) {
     LazyColumn(
         modifier = Modifier.padding(paddingValues),
@@ -55,7 +64,7 @@ private fun FeedPosts(
             top = 16.dp,
             start = 8.dp,
             end = 8.dp,
-            bottom = 72.dp
+            bottom = 16.dp
         ),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
@@ -88,6 +97,23 @@ private fun FeedPosts(
                         onCommentClickListener(feedPost)
                     }
                 )
+            }
+        }
+        item {
+            if (nextDataIsLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(color = DarkBlue)
+                }
+            } else {
+                SideEffect {
+                    viewModel.loadNextRecommendations()
+                }
             }
         }
     }
